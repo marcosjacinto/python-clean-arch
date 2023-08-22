c73494857359001ac52630b92d15da8c055be96d
marcos <marcos.gomesjacinto@gmail.com>
2023-08-22 12:58:35 -0300

    feat: implement TodoList domain model

--- src/domain.py
+++ src/domain.py
@@ -1,5 +1,5 @@
 import typing as t
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Task:
@@ -9,3 +9,22 @@ class Task:
 
     def set_as_done(self):
         self.is_done = True
+
+    def set_as_undone(self):
+        self.is_done = False
+
+@dataclass
+class TodoList:
+    tasks: t.List[Task] = field(default_factory=list)
+
+    def add_task(self, task: Task):
+        self.tasks.append(task)
+
+    def remove_task(self, task: Task):
+        self.tasks.remove(task)
+
+    def get_task(self, title: str):
+        for task in self.tasks:
+            if task.title == title:
+                return task
+        return None
--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,4 +1,4 @@
-from src.domain import Task
+from src.domain import Task, TodoList
 
 def test_task_is_created_with_correct_attributes():
     task = Task('title', 'description')
@@ -16,3 +16,35 @@ def test_set_task_as_undone():
     task.set_as_done()
     task.set_as_undone()
     assert task.is_done == False
+
+def test_create_empty_todo_list():
+    todo_list = TodoList()
+    assert todo_list.tasks == []
+
+def test_create_todo_list_with_tasks():
+    task1 = Task('title1', 'description1')
+    task2 = Task('title2', 'description2')
+    todo_list = TodoList([task1, task2])
+    assert todo_list.tasks == [task1, task2]
+
+def test_add_task_to_todo_list():
+    task = Task('title', 'description')
+    todo_list = TodoList()
+    todo_list.add_task(task)
+    assert todo_list.tasks == [task]
+
+def test_remove_task_from_todo_list():
+    task = Task('title', 'description')
+    todo_list = TodoList([task])
+    todo_list.remove_task(task)
+    assert todo_list.tasks == []
+
+def test_get_task_from_todo_list():
+    task = Task('title', 'description')
+    todo_list = TodoList([task])
+    assert todo_list.get_task('title') == task
+
+def test_get_task_from_todo_list_returns_none_if_not_found():
+    task = Task('title', 'description')
+    todo_list = TodoList([task])
+    assert todo_list.get_task('not found') == None    
